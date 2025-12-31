a5cb3c16d03c703397765b24f4b6b8ae6c42a292
yuzu09081-png <yuzu09081@gmail.com>
2025-12-31 09:12:37 +0900

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+BCDICE_API_URL=https://bcdice.onlinesession.app
+IGNORE_ERROR=1
+# export BCDICE_PASSWORD=PleaseChangeMeIfYouUseThis
+# export BCDICE_API_SECONDARY=http://secondary.bcdice-api.yourdomain.co.jp/
+# export BCDICE_DEFAULT_SYSTEM=DiceBot
+# export BCDICE_MENTION_MODE=1
+# export BCDICE_RESULT_DISPLAY_FORMAT=V1
+# export BCDICE_SLASH_PREFIX=bcdice
+# export BCDICE_SLASH_SHORT_PREFIX=br
+# export BCDICE_STANDARD_INPUT_DISABLED=disabled
+# export BCDICE_SLASH_TABLE_SUGGESTION_DISABLED=disabled
+
+java -jar discord-bcdicebot.jar "$DISCORD_BOT_TOKEN" "$BCDICE_API_URL" "$IGNORE_ERROR"
