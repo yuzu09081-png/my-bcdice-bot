5e27fad81ca8710491bc277718b581be3928a986
yuzu09081-png <yuzu09081@gmail.com>
2025-12-31 10:53:23 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# 接続先を外部の公開APIサーバーに固定します
-API_URL="https://bcdice.onlinesession.app"
+# 外部の公開APIサーバーを指定します
+BCDICE_API_URL="https://bcdice.onlinesession.app"
 IGNORE_ERROR=1
 
-# 直接引数にトークンとURLを流し込みます
-java -jar discord-bcdicebot.jar "$BOT_TOKEN" "$API_URL" "$IGNORE_ERROR"
+# 引数としてトークンとURLを渡して実行します
+java -jar discord-bcdicebot.jar "$BOT_TOKEN" "$BCDICE_API_URL" "$IGNORE_ERROR"
